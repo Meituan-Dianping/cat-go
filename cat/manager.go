75cddfc5fd8bb3a7a526d9debc64850318cdf47f
stdrickforce <stdrickforce@gmail.com>
2019-01-16 14:57:21 +0800

    Switch to absolute import

--- cat/manager.go
+++ cat/manager.go
@@ -5,7 +5,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"../message"
+	"github.com/Meituan-Dianping/cat-go/message"
 )
 
 type catMessageManager struct {
--- cat/sender.go
+++ cat/sender.go
@@ -1,11 +1,12 @@
 package cat
 
 import (
-	"../message"
 	"bytes"
 	"encoding/binary"
 	"net"
 	"time"
+
+	"github.com/Meituan-Dianping/cat-go/message"
 )
 
 func createHeader() *message.Header {
